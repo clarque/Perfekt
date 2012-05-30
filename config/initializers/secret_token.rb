bb7bbb1e735b9c49a19aaedf212d27da71caeeb6
Paul Duffy <clarque@gmail.com>
2012-05-29 20:41:21 -0700

    initialize 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Perfekt::Application.config.secret_token = 'dbe9f0e246a8c3caf656f916b3786d9fa9f31c0d9dc0765c43ddd7721122fdf0605f6a0b51ba6078637d042fe4b475822c203a2ff1741caf5b4c6a96af9e78cd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Perfekt::Application.config.session_store :cookie_store, key: '_Perfekt_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Perfekt::Application.config.session_store :active_record_store
