a90a1f49abcbf9f53b0b6cc3051c452ca14bd5a1
Paul Duffy <clarque@gmail.com>
2012-06-14 22:07:34 -0700

    occurs_at format and validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,6 @@ class Event < ActiveRecord::Base
 	validates :end_time, :format => { :with => /^((([0]?[1-9]|1[0-2])(:|\.)[0-5][0-9]((:|\.)[0-5][0-9])?( )?(AM|am|aM|Am|PM|pm|pM|Pm))|(([0]?[0-9]|1[0-9]|2[0-3])(:|\.)[0-5][0-9]((:|\.)[0-5][0-9])?))$/,
     	:message => "Times must be in a format like 18:00 or 6:00pm" }
 
+    validates :occurs_at, :presence => true
+
 end
